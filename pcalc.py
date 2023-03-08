bfb9060fbbb004df27b1138575cecb4791cd5de2
Rayhan Stewart <rayhan.stewart19@gmail.com>
2023-03-07 21:35:51 -0500

    Seemed to have squashed most of the bugs. Core module may be left that way for now, I'll add documentation soon to better use it.

--- pcalc.py
+++ pcalc.py
@@ -2,98 +2,168 @@ import sys
 import math as m
 import re
 
-# Class to store the value and unit of a number, with the option to add a descriptor
+# TODO add strict standards for how errors are handled, for example sysexit or return or raise
+numbers = []
+restricted = ["N", "kg", "s", "m", "m/s", "m/s^2",
+              "s^2", "kgm/s^2", "cos", "sin", "tan", "sqrt", "J"]
+prep_vars = ["g", "pi", "e"]  # TODO Integrate prep vars into program
 
-nums = []
+
+# TODO standard for equation input that is easy to specifically match space between num-unit, trig-num,
+#  and every operator
 
 
 class PhysicsValue:
+    """Class to store the value and unit of a number, with the option to add a descriptor
+    \n   num: value of the number
+    \n   unit: unit of the number
+    \n   name: descriptor of the number, auto generated typically by the unit
+    \n   returns: PhysicsValue object
+    """
+
     def __init__(self, num, unit, name=None):
         self.val = num
         if type(unit) == str:
             self.unit = Unit(unit)
         elif type(unit) == Unit:
             self.unit = unit
-        elif unit == None:
+        elif unit is None:
             self.unit = Unit(None)
         else:
             print("Error: Unit must be a string or a Unit object")
             sys.exit(1)
-        if unit == None:
+        if unit is None:
             self.name = "unk: " + str(hash)
 
         self.name = name
-        if self.name == None:
+        if self.name is None:
             self.name = self.unit.determine() + ": " + str(hash)
+
     # Fix operators to work with units
 
     def __repr__(self):
-        if self.unit.combined == None:
-            return (str(self.val))
-        return (str(self.val) + " " + self.unit.combined)
+        if self.unit.combined is None:
+            return str(self.val)
+        return str(self.val) + " " + self.unit.combined
 
     def __add__(self, other):
         if type(other) == int | float:
             other = PhysicsValue(other, None)
-        return (PhysicsValue(self.val + other.val, self.unit + other.unit))
+        return PhysicsValue(self.val + other.val, self.unit + other.unit)
 
     def __sub__(self, other):
         if type(other) == int | float:
             other = PhysicsValue(other, None)
-        return (PhysicsValue(self.val - other.val, self.unit + other.unit))
+        return PhysicsValue(self.val - other.val, self.unit + other.unit)
 
     def __mul__(self, other):
         if type(other) == int | float:
             other = PhysicsValue(other, None)
-        return (PhysicsValue(self.val * other.val, self.unit * other.unit))
+        return PhysicsValue(self.val * other.val, self.unit * other.unit)
 
     def __truediv__(self, other):
         if type(other) == int | float:
             other = PhysicsValue(other, None)
-        return (PhysicsValue(self.val / other.val, self.unit / other.unit))
+        return PhysicsValue(self.val / other.val, self.unit / other.unit)
 
     def __pow__(self, other):
-        return (PhysicsValue(self.val ** other.val, self.unit ** other.val))
+        return PhysicsValue(self.val ** other.val, self.unit ** other.val)
 
     def __eq__(self, other):
         # TODO add code to find missing variable and solve for it
         pass
 
     def cos(self):
-        return (m.cos(m.radians(self.val)))
+        return PhysicsValue((m.cos(m.radians(self.val))), self.unit)
 
     def sin(self):
-        return (m.sin(m.radians(self.val)))
+        return PhysicsValue((m.sin(m.radians(self.val))), self.unit)
 
     def tan(self):
-        return (m.tan(m.radians(self.val)))
+        return PhysicsValue((m.tan(m.radians(self.val))), self.unit)
 
     def sqrt(self):
-        return (m.sqrt(self.val))
+        return PhysicsValue((m.sqrt(self.val)), self.unit)  # TODO Add square root function on unit
 
     def get_num(self):
-        return (float(self.val))
+        return float(self.val)
 
     def get_unit(self):
-        return (str(self.unit))
+        return str(self.unit)
 
     def get_component(self):
-        return (str(self.unit.component))
+        return str(self.unit.component)
+
 
 # Function to interpret a string as an equation using regex
+# TODO add error handling in case of invalid input in the equation function
+# TODO Add error handling for invalid units / unit combinations / variables - evaluate rest of equation if possible
 
 
-def calc(eq):
+class PhysicsVar(PhysicsValue):
+    """Class that inherits from physicsval, and provides a way to store variables.
+        \nIn the future, this class will be able to store results of functions
+        \nand replace as values in an equation"""
 
-    priority = re.compile(r'\((?:[^()]+|\((?:[^()]+|\([^()]*\))*\))*\)') # TODO add support for nested parenthesis
-    # TODO add better support for variables, and add support to multiply numbers prior to a parenthesis or variable
+    def __init__(self, num, unit, name):
+        if name is None:
+            print("Error: Variable must have a name")
+            sys.exit(1)
+        elif type(name) != str:
+            print("Error: Variable name must be a string")
+            sys.exit(1)
+        elif name in numbers or name in restricted:
+            print("Error: Variable name already exists")
+            sys.exit(1)
+        super().__init__(num, unit, name)
+        numbers.append(self)
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if type(other) == str:
+            return self.name == other
+        elif self.name == other.name:
+            return True
+        else:
+            return False
+
+    def substitute(self, other):
+        if type(other) == int | float:
+            other = PhysicsValue(other, None)
+        self.val = other.val
+        self.unit = other.unit
+        self.name = other.name
+        return self  # Integrate this with equation function
+
+
+def calc(eq, prefix=None):
+    ''' Function to calculate the value of an equation \n   eq: string of equation to be calculated \n   returns:
+    float of the value of the equation \n   variables must be in the form of a string with no spaces, and listed in
+    the nums list - look at physicsvar class for more info
+    '''
+
+    priority = re.compile(
+        r'(?P<prefix>cos|sin|tan|v|sqrt|d+(?:\.\d+)?)?(?P<parent>\((?:[^()]+|\((?:[^()]+|\([^()]*\))*\))*\))')
+    # TODO detect square roots and other functions before parenthesis with non capturing group
+    # TODO add support to multiply numbers prior to a parenthesis or variable
+    # TODO add support for equal sign assignment of variables and calculations of variables
     for x in priority.findall(eq):
-        eq = eq.replace(x, str(calc(x[1:-1])))
-    for x in nums:
+        if x[0] is None:
+            eq = eq.replace(''.join(x), str(calc(x[1][1:-1])))
+        else:  # TODO why are there false flags for the prefix
+            eq = eq.replace(''.join(x), str(calc(x[1][1:-1], x[0])))
+    for x in numbers:  # TODO add support for variables
         if x in eq:
             eq = eq.replace(x, str(x))
     print(eq)
-    pattern = r"(?P<numbers>\d+(?:\.\d+)?)\s*(?P<units>(?<!cos|sin|tan)(?:N|kg|m|s|J|\^2)*/?(?:N|kg|m|s|J|\^2)+)?\s*(?P<trig>cos|sin|tan)?\s*(?P<trig_num>\d+(?:\.\d+)?)?\s*(?P<arithmetic>[-+*/][*]*)?\s*" #fix to not need the space in between the operator and stuff
+    # fix to not need the space in between the operator and stuff
+    # TODO Possibly re-engineer the process of extracting data
+    # Don't know yet
+
+    pattern = r"(?P<numbers>\d+(?:\.\d+)?)\s*(?P<units>(?:N|kg|m|s|J|\^2)*/?(?:N|kg|m|s|J|\^2)+)?\s*(?P<arithmetic>[" \
+              r"-+*/][*]*)?\s*"
     # Pattern requires exponents to be written as **, not ^
     extracted_data = []
     matches = re.findall(pattern, eq)
@@ -107,29 +177,16 @@ def calc(eq):
                     extracted_data.append(
                         PhysicsValue(float(group[0]), group[1]))
             elif x == group[2]:
-                if group[3] == "":
+                if group[2] == "":
                     pass
-                elif group[3] != "":
-                    temp = PhysicsValue(float(group[3]), None)
-                    print(temp)
-                    if group[0] == "":
-                        extracted_data.append(PhysicsValue(1, None))
-                    extracted_data.append("*")
-                    extracted_data.append(PhysicsValue(getattr(temp, group[2])(), None))
-                else:
-                    pass
-            elif x == group[4]:
-                if group[4] == "":
-                    pass
-                elif group[4] == "^":
+                elif group[2] == "^":
                     extracted_data.append("**")
                 else:
-                    extracted_data.append(group[4])
-    # print(extracted_data)
-    # combine all operations in extracted_data into one final result
+                    extracted_data.append(group[2])
+    print(extracted_data)
+    # combines all operations in extracted_data into one final result
     operator = ["**", "*", "/", "+", "-"]
-    # Fix to operate one operation at a time
-
+    result = None
     for y in operator:
         while extracted_data.count(y) > 0:
             x = extracted_data[extracted_data.index(y)]
@@ -145,114 +202,132 @@ def calc(eq):
                     x = "__add__"
                 elif x == "-":
                     x = "__sub__"
-                value1 = extracted_data[pos-1]
-                value2 = extracted_data[pos+1]
+                else:
+                    break
+                value1 = extracted_data[pos - 1]
+                value2 = extracted_data[pos + 1]
                 result = value1.__getattribute__(x)(value2)
                 extracted_data[extracted_data.index(value1)] = result
                 extracted_data.remove(extracted_data[pos])
                 extracted_data.remove(value2)
+    if result is None:
+        result = extracted_data[0]
+    if prefix is not None and prefix != "":
+        prefix = prefix.lower()
+        if prefix == "v":
+            prefix = "sqrt"
+        result = result.__getattribute__(prefix)()
+    return result
 
-        # if extracted_data
-        # operation = extracted_data[]
-        # value = extracted_data[i+1]
-        # result = result.__getattribute__(operation)(value)
-    return (result)
+
 # Class to store the unit of a number, with the option to add a descriptor
 
 
+def parse_unit(unit):
+    # print(unit)
+    units = re.compile(r'(?!None)(N|kg|m|s|J|\^2)')
+    if unit == None:
+        return None, None
+    if "/" in unit:
+        unit = unit.split("/")
+        numerator = units.findall(unit[0])
+        denominator = units.findall(unit[1])
+    else:
+        numerator = units.findall(unit)
+        denominator = []
+    # print(numerator, denominator)
+    numerator.sort()
+    denominator.sort()
+    for x in numerator:
+        match x:
+            case "^2":
+                numerator[numerator.index(
+                    x)] = numerator[numerator.index(x) - 1]
+            case "N":
+                numerator.remove(x)
+                numerator += ("m", "kg")
+                denominator += ("s", "s")
+            case "J":
+                numerator.remove(x)
+                numerator += ("kg", "m", "m")
+                denominator += ("s", "s")
+            case _:
+                pass
+    for x in denominator:
+        match x:
+            case "^2":
+                denominator[denominator.index(
+                    x)] = denominator[denominator.index(x) - 1]
+            case "N":
+                denominator.remove(x)
+                denominator += ("m", "kg")
+                numerator += ("s", "s")
+            case "J":
+                denominator.remove(x)
+                denominator += ("m", "m", "kg")
+                numerator += ("s", "s")
+            case _:
+                pass
+    if denominator == '' or denominator is None or denominator == []:
+        denominator = None
+    elif numerator == denominator:
+        numerator = None
+        denominator = None
+    return numerator, denominator
+
+
 class Unit:
+    """class to store the unit of a number, evaluate for validity, and combine units\n
+    unit: string of the unit to be stored, using the following format:\n
+    N, kg, m, s, J, or ^2\n
+    Can be combined with / to represent a fraction\n
+    Example: N/kg, m/s^2, J/N, etc.\n
+    combined = string of the combined unit\n
+    component = suspected component of the equation\n
+    """
+
     def __init__(self, unit):
-        self.numerator, self.denominator = self.parse_unit(unit)
+        self.numerator, self.denominator = parse_unit(unit)
         self.combined = self.combine_units()
         self.component = self.determine()
 
     def __str__(self):
-        return(self.combined) # TODO find and squash bug that causes repeated units
-    
-    def parse_unit(self, unit):
-        #print(unit)
-        units = re.compile(r'(?!None)(N|kg|m|s|J|\^2)')
-        if unit == None:
-            return (None, None)
-        if "/" in unit:
-            unit = unit.split("/")
-            numerator = units.findall(unit[0])
-            denominator = units.findall(unit[1])
-        else:
-            numerator = units.findall(unit)
-            denominator = []
-        # print(numerator, denominator)
-        numerator.sort()
-        denominator.sort()
-        for x in numerator:
-            match x:
-                case "^2":
-                    numerator[numerator.index(
-                        x)] = numerator[numerator.index(x) - 1]
-                case "N":
-                    numerator.remove(x)
-                    numerator += ("m", "kg")
-                    denominator += ("s", "s")
-                case "J":
-                    numerator.remove(x)
-                    numerator += ("kg", "m", "m")
-                    denominator += ("s", "s")
-                case _:
-                    pass
-        for x in denominator:
-            match x:
-                case "^2":
-                    denominator[denominator.index(
-                        x)] = denominator[denominator.index(x) - 1]
-                case "N":
-                    denominator.remove(x)
-                    denominator += ("m", "kg")
-                    numerator += ("s", "s")
-                case "J":
-                    denominator.remove(x)
-                    denominator += ("m", "m", "kg")
-                    numerator += ("s", "s")
-                case _:
-                    pass
-        if denominator == '' or denominator == None or denominator == []:
-            denominator = None
-        elif numerator == denominator:
-            numerator = None
-            denominator = None
-        return (numerator, denominator)
-    # Combine the numerator and denominator lists into a single string
+        # TODO find and squash bug that causes repeated units
+        return self.combined
 
     def combine_units(self):
-        if self.denominator == None:
-            if self.numerator == None:
-                return ("")
-            return (''.join(self.numerator))
+        if self.denominator is None:
+            if self.numerator is None:
+                return ""
+            return ''.join(self.numerator)
         self.numerator.sort()
         self.denominator.sort()
         # Remove common elements
         for i in self.numerator:
             if i in self.denominator:
-                    self.numerator.remove(i)
-                    self.denominator.remove(i)
+                self.numerator.remove(i)
+                self.denominator.remove(i)
         for i in self.denominator:
             if i in self.numerator:
-                    self.denominator.remove(i)
-                    self.numerator.remove(i)
-                    
+                self.denominator.remove(i)
+                self.numerator.remove(i)
+
         # print(self.numerator, self.denominator)
         # Check if list is empty
-        if self.denominator == None or self.denominator == '' or self.denominator == []:
-            if self.numerator == None or self.numerator == '' or self.numerator == []:
-                return ("")
-            return (''.join(self.numerator))
+        if self.denominator is None or self.denominator == '' or self.denominator == []:
+            if self.numerator is None or self.numerator == '' or self.numerator == []:
+                return ""
+            return ''.join(self.numerator)
         elif (self.numerator == ["kg", "m", "m"]) & (self.denominator == ["s", "s"]):
-            return ("J")
+            return "J"
         elif (self.numerator == ["kg", "m"]) & (self.denominator == ["s", "s"]):
-            return ("N")
+            return "N"
         else:
-            return (''.join(self.numerator) + "/" + ''.join(self.denominator))
+            return ''.join(self.numerator) + "/" + ''.join(self.denominator)
+
     # Determine the type of unit
 
+    # TODO Fix comparison / arithmetic to work with "" instead of none
     def determine(self):
         if self.combined == "N":
             return ("force")
@@ -276,54 +351,60 @@ class Unit:
             return ("unknown")
 
     def get_nume(self):
-        if self.numerator == None:
-            return ("")
+        if self.numerator is None:
+            return ""
         elif type(self.numerator) == list:
-            return (''.join(self.numerator))
+            return ''.join(self.numerator)
         elif type(self.numerator) == str:
-            return (self.numerator)
-    
+            return self.numerator
+
     def get_deno(self):
-        if self.denominator == None:
-            return ("")
+        if self.denominator is None:
+            return ""
         elif type(self.denominator) == list:
-            return (''.join(self.denominator))
+            return ''.join(self.denominator)
         elif type(self.denominator) == str:
-            return (self.denominator)
+            return self.denominator
 
     def __mul__(self, other):
-        if other == None:
-            return (self)
+        if other is None:
+            return self
         nums = self.get_nume() + other.get_nume()
         dens = self.get_deno() + other.get_deno()
         return Unit(nums + "/" + dens)
 
     def __truediv__(self, other):
-        if other == None:
-            return (self)
+        if other is None:
+            return self
         nums = self.get_nume() + other.get_deno()
         dens = self.get_deno() + other.get_nume()
         return Unit(nums + "/" + dens)
-    
+
     def __add__(self, other):
-        if other == None:
-            return (self)
-        elif other != None:
+        if other is None:
+            return self
+        elif self.combine_units() == "":
+            return other
+        elif other is not None:
             if self.combine_units() == other.combine_units():
-                return (self)
+                return self
             else:
                 raise ValueError("Units do not match")
 
     def __pow__(self, other):
         if other == 0:
-            return (Unit(" "))
+            return Unit(" ")
         elif other == 1:
-            return (self)
+            return self
         elif other == 2:
-            return (Unit((self * self))) # TODO fix this
+            return Unit((self * self))  # TODO fix this
 
 
-class KinematicsEquation: # TODO brainstorm how we will do this lol
+# TODO engineer the scope of this class, including possibility of evaluating the equation to a possible equation
+#  template
+
+
+class KinematicsEquation:  # TODO brainstorm how we will do this lol
     def __init__(self):
         self.variables = []
         self.parse()
--- pctest.py
+++ pctest.py
@@ -1,37 +1,30 @@
 import sys
+import os
 import pcalc as p
-import math as m
-import random as r
+
 print(f' Answer: {p.calc("1m + 3m + 4m")}')
 print(f' Answer: {p.calc("1m * 4m")}')
 print(f' Answer: {p.calc("1m / 2m")}')
 print(f' Answer: {p.calc("2m * 3m")}')
-print(f' Answer: {p.calc ("(1m + 2m + 3m) / 10s")}')
-#print(f' Answer: {p.calc("10m/s ** 2")} ') # ERROR FIX LATER
+print(f' Answer: {p.calc("(1m + 2m + 3m) / 10s")}')
+# print(f' Answer: {p.calc("10m/s ** 2")} ') # ERROR FIX LATER
 print(f' Answer: {p.calc("10m/s^2 * 2s")}')
 print(f' Answer: {p.calc("10m/s^2 / 2m/s * 3m")}')
 print(f' Answer: {p.calc("10kgm/s^2 + 2N")}')
 # Generate test cases
-print("Generating test cases")
-'''testcases = []
-units = ["m", "s", "kg", "m/s", "m/s2", "s2", "kgm/s2"]
-operators = ["*", "/"]
-for x in range(0, 10):
-    testcases.append(p.calc(str(m.floor(r.random()*100)) + r.choice(units) + r.choice(operators) + str(m.floor(r.random()*100)) + r.choice(units)))
 
-for x in testcases:
-    print(x)'''
 equations = []
 print(p.calc("10 cos 30"))
-print(p.calc("25kg * (0.5 * (10m/s^2 * 2s))")) #TODO ERROR FIX LATER
-print( p.calc("20 * 20 * 20 + 2"))
+print(p.calc("25kg * (0.5 * (10m/s^2 * 2s))"))  # TODO ERROR FIX LATER
+print(p.calc("20 * 20 * 20 + 2"))
+print(p.calc("10 + 10s"))
+os.system("cls")
 while True:
     try:
-        num = p.calc(input("\nEnter your calculation: \n"))
+        num = p.calc(input("\nEnter your calculation: "))
         print(str(num), "  ", num.get_component(), "\n")
     except KeyboardInterrupt:
         print(" Goodbye!")
         sys.exit(0)
-    except:
-        print("Error: Invalid input")
+    finally:
         continue
